380492aae77bb41d6259cb0b714183ebe7a799c9
Soumya Annie Thomas <soumyaannie07@gmail.com>
2024-11-26 23:47:10 +0530

    first commit

---
+++ 1.Basics.js
@@ -0,0 +1,20 @@
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+//                                                            21/08/24
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+//                                                      Basic Concepts in JS
+
+//1. Displaying contents
+
+console.log("This is JS Program!!!");
+console.log(typeof "This is JS Program!!!");
+console.log('T');
+console.log(typeof 'T');
+console.log(1000);
+console.log(typeof 1000);
+console.log(1000.890);
+console.log(typeof 1000.890);  
+console.log(true);
+console.log(typeof true);
+console.log(false);
+console.log(typeof false);
