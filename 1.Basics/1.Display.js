63c698b8af2a83ea5517535f699d453696ea3b82
Soumya Annie Thomas <soumyaannie07@gmail.com>
2024-11-27 05:43:13 +0530

    Day1

--- 1.Basics/1.Display.js
+++ 1.Basics/1.Display.js
@@ -4,7 +4,7 @@
 
 //                                                      Basic Concepts in JS
 
-//1. Displaying contents
+//Displaying contents, Typeof, Datatypes,Combining different datatypes
 
 console.log("This is JS Program!!!");
 console.log(typeof "This is JS Program!!!");
@@ -18,3 +18,6 @@ console.log(true);
 console.log(typeof true);
 console.log(false);
 console.log(typeof false);
+
+console.log("ECMAScript", 6); //6 is number
+console.log("ECMAScript "+6); //6 is string
---
+++ 1.Basics/2.Variables.js
@@ -0,0 +1,12 @@
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+//                                                            21/08/24
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+//                                                      Variables
+var firstData
+console.log(firstData, typeof firstData);
+var firstData = 100
+console.log(firstData, typeof firstData);
+var firstData = 'JS'
+console.log(firstData, typeof firstData);
+
