d48cc7bfe780477b0f8d7ac43f40b24875c0d2c0
Soumya Annie Thomas <soumyaannie07@gmail.com>
2024-11-28 06:25:33 +0530

    Day2

--- 1.Basics/2.Variables.js
+++ 1.Basics/2.Variables.js
@@ -1,12 +1,48 @@
 // ---------------------------------------------------------------------------------------------------------------------------------------------
-//                                                            21/08/24
+//                                                                  21/08/24
 // ---------------------------------------------------------------------------------------------------------------------------------------------
 
-//                                                      Variables
-var firstData
+//                                                                  Variables
 console.log(firstData, typeof firstData);
+var firstData                               //Hoisiting is happening
+
 var firstData = 100
 console.log(firstData, typeof firstData);
 var firstData = 'JS'
 console.log(firstData, typeof firstData);
 
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                  22/08/24
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+const secondData = "const vaariable" //Hoisting is not supported - Error (ReferenceError: Cannot access 'secondData' before initialization)
+console.log(secondData);
+
+// secondData = 100 -   Error (TypeError: Assignment to constant variable.)
+
+var i = 10
+
+// Here we combine different data types using Temperal literals
+
+console.log(`Before loop, i = ${i}`); //10
+console.log(`Inside loop`); 
+for (let i = 1; i <= 5;i++){
+    console.log(i);
+}
+
+console.log(`After loop, i = ${i}`); //10
+
+var i = 10
+console.log(`Before loop, i = ${i}`); //10
+console.log(`Inside loop`); 
+for (var i = 1; i <= 5;i++){
+    console.log(i);
+}
+
+console.log(`After loop, i = ${i}`); //6
+
+
+let j = 10
+console.log(j);//Hoisting is not supported - Error (ReferenceError: Cannot access 'j' before initialization)
+
+console.log(`Variables are firstData = ${firstData}, secondData = ${secondData} and i = ${i}`);
---
+++ 1.Basics/3.Operators.js
@@ -0,0 +1,44 @@
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                  22/08/24
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+//                                                             Arithmetic Operators
+
+//  - Modulus (%)
+
+var x = 10
+console.log(`${x} % 3 = ${x % 3}`);
+
+var y = 12345
+//print the last digit of y
+console.log(`Last digit of ${y} = ${y % 10}`);
+
+//  - Double Asterisks (**)
+
+//to find the power of a number
+
+console.log(`${x}^2 = ${x ** 2}`);
+
+//                                                             Relational Operators
+//                                                          (Always returns Boolean values)
+
+console.log(`Is x greater than y : ${x > y}`);
+
+z = '10'
+console.log(`when x = ${x}(number)  & z= ${z}(string)`);
+console.log(`is x equals z : ${x == z}`);
+
+z = '14'
+console.log(`when x = ${x}(number) & z= ${z}(string)`);
+console.log(`is x equals z : ${x == z}`);
+
+z = '10'
+console.log(`when x = ${x}(number) & z= ${z}(string)`);
+console.log(`is x equals z : ${x === z}`);
+
+//                                                             Logical Operators
+//                                                (To combine Realtional Expressions, Always returns Boolean)
+
+w = 20
+//check x is in b/w w & y
+console.log(`Is x (x=${x}) is in between w (w=${w}) and y (y=${y}) : ${x < y && x > w}`);
