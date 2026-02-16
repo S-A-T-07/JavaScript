c4d36a904af8fa2463c1fa869ee8773586453a21
Soumya Annie Thomas <soumyaannie07@gmail.com>
2026-02-16 23:37:03 +0530

    edited

--- 5.Array/5.PrintDuplicate.js
+++ 5.Array/5.PrintDuplicate.js
@@ -1,15 +1,16 @@
-// Print Duplicate Element 
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+//                                                                      04/09/24
+// ---------------------------------------------------------------------------------------------------------------------------------------------
 
-duplicateArray = [10, 20, 10, 20, 11, 12]
+// Print Duplicate Element
+
+duplicateArray = [10, 20, 10, 20, 11, 12];
 
 for (let i = 0; i < duplicateArray.length; i++) {
-
-    for (let j = i+1; j < duplicateArray.length; j++){
-        
-        if (duplicateArray[i] == duplicateArray[j]) {
-            console.log(duplicateArray[i]);
-            break;    
-        }
+  for (let j = i + 1; j < duplicateArray.length; j++) {
+    if (duplicateArray[i] == duplicateArray[j]) {
+      console.log(duplicateArray[i]);
+      break;
     }
-
+  }
 }
